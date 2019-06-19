78af336bac64fb56ddbb784a77945ed23e10a7a3
Tago Trotta <mrtrotta2010@gmail.com>
2019-06-19 20:59:45 -0300

    refacoring...

--- IA/Wumpus/worldofwumpus.py
+++ IA/Wumpus/worldofwumpus.py
@@ -5,29 +5,10 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 import sys
-import _thread as thread
-
-from playsound import playsound
-
-def soundFX(arquivo, loop):
-
-	if loop:
-		while (True):
-			playsound(arquivo)
-
-	else:
-		playsound(arquivo)
-
-def fechar (self, widget):
-	
-	thread.exit()
-	Gtk.main_quit()
 
 if __name__ == "__main__":
 
 	worldOfWumpus = JanelaPrincipal(sys.argv[1])
-	worldOfWumpus.connect("delete-event", fechar)
+	worldOfWumpus.connect("delete-event", Gtk.main_quit)
 	worldOfWumpus.show_all()
-
-	thread.start_new_thread(soundFX, ('Audio/March of the Spoons.mp3', True))
 	worldOfWumpus.main()
--- SO/main.c
+++
@@ -1,9 +0,0 @@
-#include "shellso.h"
-
-int main(int argc, char *argv[]){
-
-	shell(argc, argv);
-
-    return 0;
-}
-
